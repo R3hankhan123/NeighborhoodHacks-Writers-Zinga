dade3c5e74b9e4df52e07598adddbdd015d77d13
Saksham Singh <sakshamsingh@Sakshams-MacBook-Air.local>
2022-09-09 09:31:56 +0530

    Made Login and Register paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Background from './components/Background/Background';
+
 import Navbar from './components/Navbar/Navbar';
-import Body from './components/Body/Body';
-import { Routes, Route, Link } from "react-router-dom";
+
+import {BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -19,11 +20,13 @@ function App() {
 
 <Navbar />
 
+
 <Routes>
 <Route path='/' element={<Home/>}></Route>
 <Route path='/login' element={<Login/>}></Route>
-
+<Route path='/register' element={<Register/>}></Route>
 </Routes>
+
 
     </div>
   );
--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,14 +1,10 @@
-import React, { Fragment, useState } from 'react'
-import './Login.css'
+import React, {useState} from 'react'
+import "./Register.css"
 import {Button, Grid,Link,Paper, Typography} from '@mui/material'
 import { TextField } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 
-
-
-
-
-function Login() {
+function Register() {
     const theme = createTheme({
         status: {
           danger: '#e53e3e',
@@ -33,20 +29,27 @@ e.preventDefault()
        }
 
     const paperStyle={padding:20,height:'70vh',width:450,margin:"20px auto"}
-
     return (
-
         <Grid>
             <Paper elevation={10} style={paperStyle}>
-                <Typography variant='h2' sx={{fontFamily:"Roboto"}} mt={2} mb={4} ml={16}>Sign In</Typography>
+                <Typography variant='h2' sx={{fontFamily:"Roboto"}} mt={2} mb={4} ml={16}>Sign Up</Typography>
             <Grid>
             <form onSubmit={loginHandler}>
+            <TextField
+          required
+          id="outlined-required"
+          label="Name"
+          fullWidth
+          mt={2} mb={4}
+          
+        />
             <TextField
           required
           id="outlined-required"
           label="Email"
           fullWidth
           mt={2} mb={4}
+          sx={{marginTop:"5px"}}
           
         />
          <TextField
@@ -58,14 +61,23 @@ e.preventDefault()
           sx={{marginTop:"5px"}}
           
         />
+         <TextField
+          required
+          id="outlined-required"
+          label="Confirm Password"
+          fullWidth
+          type='password'
+          sx={{marginTop:"5px"}}
+          
+        />
 <ThemeProvider theme={theme}><Button color='neutral' variant="contained" fullWidth type='submit'
  sx={{marginTop:"20px"}}
->Sign In</Button></ThemeProvider>
+>Register</Button></ThemeProvider>
             </form>
             </Grid>
             <ThemeProvider theme={theme}>
-            <Typography mt={1}><Link underline="hover" color="#42a5f5">Forgot Password?</Link></Typography>
-            <Typography ml={14} mt={10}>Don't have an account? <Link href='/register' color="#42a5f5" underline="hover">Sign Up</Link></Typography>
+
+            <Typography ml={14} mt={5}>Already have an account? <Link href='/login' color="#42a5f5" underline="hover">Sign In</Link></Typography>
             </ThemeProvider> 
             </Paper>
            
@@ -73,4 +85,4 @@ e.preventDefault()
     )
 }
 
-export default Login
+export default Register
